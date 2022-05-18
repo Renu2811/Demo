725c3c28cf010ed56149df3ba484a24cd4a322e3
Renu2811 <91143736+Renu2811@users.noreply.github.com>
2022-05-17 20:02:19 -0700

    Add files via upload
    adding files based on oops concept

---
+++ User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsConcept
+{
+
+    //creating method for getting user id and name
+    public  class User
+    {
+        public int id = 0;
+        public string name = string.Empty;
+      
+        public void GetUserDetails(int uid, string uname)
+        {
+            id = uid;
+            name = uname;
+            Console.WriteLine("Id: {0},Name: {1}", id, name);
+
+            
+        }
+
+        
+    }
+}
---
+++ UserDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsConcept
+{
+    public  class UserDetails
+    {
+        public string Name;
+        public int Age;
+        public UserDetails(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public void GetUser()
+        {
+            Console.WriteLine("Name: {0}, Age: {1}", Name, Age);
+        }
+    }
+}
+
